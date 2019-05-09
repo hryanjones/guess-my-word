89034eba39b7a00e675d7c3b45b20e6be1a20158
H. Ryan Jones <hrjones@amazon.com>
2019-05-09 11:43:15 -0700

    1. Add linting, 2. Don't allow duplicates, 3. Store guesses instead of number of guesses.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const possibleWords = {
     // normal words were from 1-1,000 common English words on TV and movies https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/TV/2006/1-1000
     normal: /* DON'T LOOK CLOSELY UNLESS YOU WANT TO BE SPOILED!!! */['', '', '', 'course', 'against', 'ready', 'daughter', 'work', 'friends', 'minute', 'though', 'supposed', 'honey', 'point', 'start', 'check', 'alone', 'matter', 'office', 'hospital', 'three', 'already', 'anyway', 'important', 'tomorrow', 'almost', 'later', 'found', 'trouble', 'excuse', 'hello', 'money', 'different', 'between', 'every', 'party', 'either', 'enough', 'year', 'house', 'story', 'crazy', 'mind', 'break', 'tonight', 'person', 'sister', 'pretty', 'trust', 'funny', 'gift', 'change', 'business', 'train', 'under', 'close', 'reason', 'today', 'beautiful', 'brother', 'since', 'bank', 'yourself', 'without', 'until', 'forget', 'anyone', 'promise', 'happy', 'bake', 'worry', 'school', 'afraid', 'cause', 'doctor', 'exactly', 'second', 'phone', 'look', 'feel', 'somebody', 'stuff', 'elephant', 'morning', 'heard', 'world', 'chance', 'call', 'watch', 'whatever', 'perfect', 'dinner', 'family', 'heart', 'least', 'answer', 'woman', 'bring', 'probably', 'question', 'stand', 'truth', 'problem',],
@@ -5,14 +6,18 @@ const possibleWords = {
     // hard words were gotten from a top 100 SAT word list https://education.yourdictionary.com/for-students-and-parents/100-most-common-sat-words.html
     hard: /* DON'T LOOK CLOSELY UNLESS YOU WANT TO BE SPOILED!!! */                                      ['abdicate','empathy','abate','venerable','exemplary','hackneyed','foster','aberration','clairvoyant','extenuating','mundane','forbearance','fortitude','prudent','hypothesis','ephemeral','scrutinize','capitulate','spurious','substantiate','intuitive','tenacious','digression','prosperity','compromise','vindicate','fraught','submissive','ostentatious','boisterous','bias','impetuous','wary','rancorous','deleterious','amicable','reclusive','canny','superficial','emulate','frugal','perfidious','jubilation','brusque','intrepid','sagacity','arid','inconsequential','nonchalant','reconciliation','brazen','prosaic','pretentious','benevolent','aesthetic','adversity','abhor','divergent','fortuitous','conditional','disdain','demagogue','asylum','compassion','hedonist','condescending','querulous','collaborate','inevitable','discredit','renovation','lobbyist','enervating','provocative','florid','convergence','subtle','diligent','surreptitious','orator','superfluous','opulent','capacious','tactful','longevity','restrained','conformist','abstain','pragmatic','reverence','spontaneous','anachronistic','haughty','procrastinate','parched','camaraderie','precocious','evanescent','impute','transient', ],
 };
+/* eslint-enable */
+
 const WIN = 'win';
 const BEFORE = 'before';
 const AFTER = 'after';
-let numberOfGuesses;
+let guesses = [];
 let startTime;
 let word;
-let timezonelessDate; // will be used for leaderboard
 let difficulty = 'normal';
+
+// will be used for leaderboard
+let timezonelessDate; // eslint-disable-line no-unused-vars
 
 setup();
 
@@ -29,7 +34,7 @@ function setup() {
     input.focus();
 
     // reset stats
-    numberOfGuesses = 0;
+    guesses = [];
     startTime = null;
 
     // remove possible win/gave-up state
@@ -72,20 +77,20 @@ function getForm() {
 }
 
 function getGuesses() {
-    return document.getElementsByClassName('guess')
+    return document.getElementsByClassName('guess');
 }
 
 function getDifficultyChanger() {
     return document.getElementById('difficulty-changer');
 }
 
-function resetValidation() {
+function resetValidation() { // eslint-disable-line no-unused-vars
     const guess = getGuess();
     const invalidReason = getInvalidReason(guess);
     getInput().setCustomValidity(invalidReason);
 }
 
-function makeGuess(event) {
+function makeGuess(/* event */) { // eslint-disable-line no-unused-vars
     const guess = getGuess();
     const invalidReason = getInvalidReason(guess);
     const input = getInput();
@@ -112,7 +117,7 @@ function makeGuess(event) {
     return false;
 
     function updateStats() {
-        numberOfGuesses += 1;
+        guesses.push(guess);
         if (!startTime) {
             startTime = new Date();
         }
@@ -121,7 +126,7 @@ function makeGuess(event) {
 
 function getGuess() {
     const input = getInput();
-    return sanitizeGuess(input && input.value || '');
+    return sanitizeGuess((input && input.value) || '');
 
     function sanitizeGuess(guess) {
         return guess.toLowerCase().trim().replace(/[^a-z]/g, '');
@@ -133,13 +138,16 @@ function getInvalidReason(guess) {
         return "Guess can't be empty.";
     }
     if (!isAValidWord(guess)) {
-        return "Guess must be an English word. (Scrabble-acceptable)";
+        return 'Guess must be an English word. (Scrabble-acceptable)';
+    }
+    if (guesses.includes(guess)) {
+        return "Oops, you've already guessed that word.";
     }
     return '';
 }
 
 function isAValidWord(guess) {
-    let level = validWordTrie;
+    let level = validWordTrie; // eslint-disable-line no-undef 
     for (const letter of guess) {
         level = level[letter];
         if (!level) return false;
@@ -157,22 +165,28 @@ function getComparisonToTargetWord(guess) {
 function handleWin() {
     const stats = document.getElementById('stats');
     const winTime = new Date();
-    stats.innerText = `(${numberOfGuesses} guesses in ${getFormattedTime(startTime, winTime)})`;
+    stats.innerText = `(${guesses.length} guesses in ${getFormattedTime(startTime, winTime)})`;
     getInput().disabled = true;
     getForm().classList.add('show-win');
 }
 
 function getFormattedTime(start, end) {
     let seconds = Math.round((end - start) / 1000);
-    let hours = Math.floor(seconds / 3600);
-    seconds = seconds % 3600;
-    let minutes = Math.floor(seconds / 60);
-    seconds = seconds % 60;
+    const hours = Math.floor(seconds / 3600);
+    seconds %= 3600;
+    const minutes = Math.floor(seconds / 60);
+    seconds %= 60;
 
     const formattedTime = [];
-    hours && formattedTime.push(hours + 'h');
-    minutes && formattedTime.push(minutes + 'm');
-    seconds && formattedTime.push(seconds + 's');
+    if (hours) {
+        formattedTime.push(`${hours}h`);
+    }
+    if (minutes) {
+        formattedTime.push(`${minutes}m`);
+    }
+    if (seconds) {
+        formattedTime.push(`${seconds}s`);
+    }
 
     return formattedTime.join(' ') || '0s';
 }
@@ -182,10 +196,11 @@ function recordGuess(guess, comparison) {
     removeClass('current-guess');
 
     insertGuess(generateGuessElement(), comparison);
-    
+
     revealLabel(comparison);
 
     function generateGuessElement() {
+        const numberOfGuesses = guesses.length;
         const guessElement = document.createElement('div');
         guessElement.className = 'guess current-guess';
 
@@ -216,10 +231,10 @@ function insertGuess(guessElement, comparison) {
 }
 
 function guessIsNext(previousGuessElement, guessElement, comparison) {
-    const guess = guessElement.dataset.guess;
+    const { guess } = guessElement.dataset;
     const previousGuess = previousGuessElement.dataset.guess;
-    return (comparison === AFTER && previousGuess < guess) ||
-        (comparison === BEFORE && previousGuess > guess);
+    return (comparison === AFTER && previousGuess < guess)
+        || (comparison === BEFORE && previousGuess > guess);
 }
 
 function revealLabel(comparison) {
@@ -233,7 +248,7 @@ function removeClass(className) {
     }
 }
 
-function giveUp() {
+function giveUp() { // eslint-disable-line no-unused-vars
     if (!confirm('Really give up?')) {
         return;
     }
@@ -243,7 +258,7 @@ function giveUp() {
     getForm().classList.add('gave-up');
 }
 
-function changeDifficulty(newDifficulty) {
+function changeDifficulty(givenDifficulty) { // eslint-disable-line no-unused-vars
     const haveMadeGuesses = getGuesses().length > 0;
     const formClasses = getForm().classList;
     const haveWonOrGivenUp = formClasses.contains('show-win') || formClasses.contains('gave-up');
@@ -252,10 +267,9 @@ function changeDifficulty(newDifficulty) {
         difficultyChanger.value = difficulty;
         return;
     }
-    if (newDifficulty) {
+    const newDifficulty = givenDifficulty || difficultyChanger.value;
+    if (newDifficulty !== difficultyChanger.value) {
         difficultyChanger.value = newDifficulty;
-    } else {
-        newDifficulty = difficultyChanger.value;
     }
     difficulty = newDifficulty;
     setup();
@@ -264,7 +278,7 @@ function changeDifficulty(newDifficulty) {
 // Utilities
 
 function getTimezonelessLocalDate(date) {
-    return `${date.getFullYear()}-${getMonth(date)}-${getMonth(date)}`
+    return `${date.getFullYear()}-${getMonth(date)}-${getMonthDay(date)}`;
 }
 
 function getMonth(date) {
@@ -283,6 +297,7 @@ function leftPad(string, desiredLength, character = '0') {
 }
 
 // https://stackoverflow.com/questions/8619879/javascript-calculate-the-day-of-the-year-1-366
+/* eslint-disable */
 function isLeapYear(date) {
     var year = date.getFullYear();
     if((year & 3) != 0) return false;
@@ -298,3 +313,4 @@ function getDOY(date) {
     if(mn > 1 && isLeapYear(date)) dayOfYear++;
     return dayOfYear;
 };
+/* eslint-enable */
