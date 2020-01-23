7948f77e1e1120b8f6b0d3251b41d4a829c4f720
H. Ryan Jones <hryanjones@gmail.com>
2020-01-22 17:55:45 -0800

    fix accidental board server commit

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
-// const BOARD_SERVER = 'https://home.hryanjones.com';
-const BOARD_SERVER = 'http://192.168.0.144:8080';
+const BOARD_SERVER = 'https://home.hryanjones.com';
+// const BOARD_SERVER = 'http://192.168.0.144:8080';
 // const BOARD_SERVER = 'https://hryanjones.builtwithdark.com';
 
 // const BAD_NAMES_SERVER = 'https://hryanjones.builtwithdark.com/gmw/bad-names';
